0e7fce878a4e1e373791bbe7d4f023c9fb96786f
Mansard <nmansard@laas.fr>
2011-03-09 17:00:37 +0100

    Include mal2eig in the proper namespaces.

--- src/mal-to-eigen.h
+++ src/mal-to-eigen.h
@@ -25,48 +25,96 @@ namespace Eigen
   typedef Matrix<double,Dynamic,Dynamic,RowMajor> MatrixRXd;
 }
 
-#define EIGEN_MATRIX_FROM_SIGNAL(name,signal)	         \
-  Eigen::Map<Eigen::MatrixRXd> name			 \
-  (							 \
-   const_cast<double*>(signal.accessToMotherLib().data().begin()),	\
-   signal.nbRows(),					 \
-   signal.nbCols()					 \
-							 )
-#define EIGEN_VECTOR_FROM_SIGNAL(name,signal)	         \
-  Eigen::Map<Eigen::VectorXd> name			 \
-  (							 \
-   signal.accessToMotherLib().data().begin(),		 \
-   signal.size()					 \
-							 )
+#define EIGEN_MATRIX_FROM_SIGNAL(name,signal)	                         \
+  Eigen::Map<Eigen::MatrixRXd> name			                 \
+  (							                 \
+   const_cast<double*>(signal.accessToMotherLib().data().begin()),	 \
+   signal.nbRows(),				               	         \
+   signal.nbCols()				               	         \
+						               	         )
+#define EIGEN_VECTOR_FROM_SIGNAL(name,signal)	                         \
+  Eigen::Map<Eigen::VectorXd> name		               	         \
+  (						               	         \
+   signal.accessToMotherLib().data().begin(),	               	         \
+   signal.size()				               	         \
+						               	         )
 
-template< typename D >
-inline void EIGEN_VECTOR_TO_VECTOR( const Eigen::MatrixBase<D>& in, ml::Vector & out )
-{
-  EIGEN_STATIC_ASSERT_VECTOR_ONLY( Eigen::MatrixBase<D> );
-  out.resize(in.size());
-  memcpy( out.accessToMotherLib().data().begin(),in.derived().data(),
-	  in.size()*sizeof(double));
-}
 
-template< typename MB >
-inline void EIGEN_ROWMAJOR_MATRIX_TO_MATRIX( const Eigen::MatrixBase<MB>& in,
-					     ml::Matrix & out )
+namespace dynamicgraph
 {
-  out.resize( in.rows(),in.cols() );
-  memcpy( out.accessToMotherLib().data().begin(),in.derived().data(),
-	  in.cols()*in.rows()*sizeof(double));
-}
+  namespace sot
+  {
+    namespace dyninv
+    {
 
-template< typename MB >
-inline void EIGEN_COLMAJOR_MATRIX_TO_MATRIX( const Eigen::MatrixBase<MB>& in,
-					     ml::Matrix & out )
-{
-  // TODO: find a better way for that!
-  out.resize( in.rows(),in.cols() );
-  for( int i=0;i<in.rows();++i )
-    for( int j=0;j<in.cols();++j )
-      out(i,j)=in(i,j);
-}
+      template< typename D >
+	inline void EIGEN_VECTOR_TO_VECTOR( const Eigen::MatrixBase<D>& in, ml::Vector & out )
+	{
+	  EIGEN_STATIC_ASSERT_VECTOR_ONLY( Eigen::MatrixBase<D> );
+	  out.resize(in.size());
+	  memcpy( out.accessToMotherLib().data().begin(),in.derived().data(),
+		  in.size()*sizeof(double));
+	}
+
+      template< typename MB >
+	inline void EIGEN_ROWMAJOR_MATRIX_TO_MATRIX( const Eigen::MatrixBase<MB>& in,
+						     ml::Matrix & out )
+	{
+	  out.resize( in.rows(),in.cols() );
+	  memcpy( out.accessToMotherLib().data().begin(),in.derived().data(),
+		  in.cols()*in.rows()*sizeof(double));
+	}
+
+      template< typename MB >
+	inline void EIGEN_COLMAJOR_MATRIX_TO_MATRIX( const Eigen::MatrixBase<MB>& in,
+						     ml::Matrix & out )
+	{
+	  // TODO: find a better way for that!
+	  out.resize( in.rows(),in.cols() );
+	  for( int i=0;i<in.rows();++i )
+	    for( int j=0;j<in.cols();++j )
+	      out(i,j)=in(i,j);
+	}
+
+
+#ifdef __SOT_MultiBound_H__
+      inline void COPY_MB_VECTOR_TO_EIGEN( const VectorMultiBound& ddx,
+					   soth::VectorBound& btask1 )
+      {
+	const int nx1 = ddx.size();
+	for( int c=0;c<nx1;++c )
+	  {
+	    if( ddx[c].getMode() == MultiBound::MODE_SINGLE )
+	      btask1[c] = ddx[c].getSingleBound();
+	    else
+	      {
+		const bool binf = ddx[c].getDoubleBoundSetup( dg::sot::MultiBound::BOUND_INF );
+		const bool bsup = ddx[c].getDoubleBoundSetup( dg::sot::MultiBound::BOUND_SUP );
+		if( binf&&bsup )
+		  {
+		    const double xi = ddx[c].getDoubleBound(dg::sot::MultiBound::BOUND_INF);
+		    const double xs = ddx[c].getDoubleBound(dg::sot::MultiBound::BOUND_SUP);
+		    btask1[c] = std::make_pair( xi, xs );
+		  }
+		else if( binf )
+		  {
+		    const double xi = ddx[c].getDoubleBound(dg::sot::MultiBound::BOUND_INF);
+		    btask1[c] = soth::Bound( xi, soth::Bound::BOUND_INF );
+		  }
+		else
+		  {
+		    assert( bsup );
+		    const double xs = ddx[c].getDoubleBound(dg::sot::MultiBound::BOUND_SUP);
+		    btask1[c] = soth::Bound( xs, soth::Bound::BOUND_SUP );
+		  }
+	      }
+	  }
+      }
+#endif // #ifdef __SOT_MultiBound_H__
+
+    } // namespace dyninv
+  } // namespace sot
+} // namespace dynamicgraph
 
 
 
