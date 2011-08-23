d1994da5162649cf68b93594eda7f87dde552727
Mansard <nmansard@laas.fr>
2011-08-23 11:05:35 +0200

    Transfert all the class from the unittest to the header..

--- unitTesting/qrt.cpp
+++ unitTesting/qrt.cpp
@@ -14,107 +14,14 @@
  * with sot-dyninvc.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-#include <Eigen/QR>
+#include <sot-dyninv/col-piv-qr-solve-in-place.h>
 #include <soth/Algebra.hpp>
 #include <iostream>
 
 std::ostream& p( const char * n ) { return std::cout << n << " = "; }
 
 
-namespace Eigen
-{
 
-  /* This class add two methods to the classical ColPiv: solveInPlace for full
-   * col rank matrices (and the direct consequence of pseudoInverse for
-   * f-c-r). And solveTranposeInPlace for full row rank matrices (and the
-   * direct pseudoInverseTranspose).
-   */
-  class ColPivQRSolveInPlace
-    : public ColPivHouseholderQR< MatrixXd >
-  {
-  public:
-
-    /* Find a solution x to the problem G x = b. The input vector is b,
-     * completed by the 0 tail to obtain the x size.  The solver only works for
-     * full-col rank matrices, ie matrices G = Q [ R; 0 ], with R full-rank
-     * (full diag) upper triangular. */
-    void solveInPlace( MatrixXd& Gp )
-    {
-      const int r = rank(), m=cols(), n=rows();
-      assert( r==m );
-      assert( Gp.rows() == m ); // TODO: if not proper size, resize.
-
-      VectorXd workspace( n );
-      /* P2*P1*P0 ... */
-      for (int k = r-1; k >= 0 ; --k)
-	{
-	  int remainingSize = n-k;
-	  Gp.rightCols( Gp.cols()-k )
-	    .applyHouseholderOnTheRight(matrixQR().col(k).tail(remainingSize-1),
-					hCoeffs().coeff(k), &workspace.coeffRef(0));
-	}
-
-      matrixQR()
-	.topLeftCorner(r,r).triangularView<Upper>()
-	.solveInPlace(Gp);
-
-      Gp.applyOnTheLeft( colsPermutation() );
-    }
-
-    MatrixXd pseudoInverse( void )
-    {
-      MatrixXd res = MatrixXd::Identity( cols(),rows() );
-      solveInPlace( res );
-      return res;
-    }
-
-
-
-    /* Find a solution x to the problem G'x = b. The input vector is b,
-     * completed by the 0 tail to obtain the x size.  The solver only works for
-     * full-row rank matrices, ie matrices G' = [ L 0 ] Q, with L full-rank
-     * (full diag) lower triangular. */
-    void solveTransposeInPlace( MatrixXd& Gtp )
-    {
-      /* r is the rank, nxm the size of the original matrix (ie whose transpose
-       * has been decomposed. n is the number of cols of the original matrix,
-       * thus number of rows of the transpose we want to inverse. */
-      const int r = rank(), n=cols(), m=rows();
-      assert( r==n );
-      assert( Gtp.rows() == m ); // TODO: if not proper size, resize.
-
-      /* G E = Q R  ::  E' G' = L Q', with L=R'
-       * => G^+T = Q R^+T E' */
-
-      /* Compute E'*X. */
-      Gtp.applyOnTheLeft( colsPermutation().transpose() );
-
-      /* Compute R^+T E'*X. */
-      matrixQR()
-	.topLeftCorner(r,r).transpose().triangularView<Lower>()
-	.solveInPlace(Gtp.topRows(r));
-
-      /* Compute Q R^+T E'*X. */
-      /* Q = P1*P2* ... *Pn */
-      VectorXd workspace( n );
-      for (int k = r-1; k >= 0 ; --k)
-	{
-	  int remainingSize = m-k;
-	  Gtp.bottomRows( Gtp.rows()-k )
-	    .applyHouseholderOnTheLeft(matrixQR().col(k).tail(remainingSize-1),
-				       hCoeffs().coeff(k), &workspace.coeffRef(0));
-	}
-    }
-
-    MatrixXd pseudoInverseTranspose( void )
-    {
-      MatrixXd Gtp = MatrixXd::Identity( rows(),cols() );
-      solveTransposeInPlace( Gtp );
-      return Gtp;
-    }
-
-  };
-}
 
 int main (void)
 {
