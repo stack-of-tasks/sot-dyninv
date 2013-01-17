9e152c55e26c5662337e6a492b98ee84277bd207
Mansard <nmansard@laas.fr>
2013-01-17 16:14:11 +0100

    Added the task weight in the python init file.

--- src/dynamic_graph/sot/dyninv/__init__.py
+++ src/dynamic_graph/sot/dyninv/__init__.py
@@ -1,47 +1,24 @@
 from controller_pd import ControllerPD
-ControllerPD('')
-
 from task_dyn_pd import TaskDynPD
-TaskDynPD('')
-
 from task_dyn_inequality import TaskDynInequality
-TaskDynInequality('')
-
 from dynamic_integrator import DynamicIntegrator
-DynamicIntegrator('')
-
 from pseudo_robot_dynamic import PseudoRobotDynamic
-PseudoRobotDynamic('')
-
 from solver_op_space import SolverOpSpace
-SolverOpSpace('')
 from solver_dyn_reduced import SolverDynReduced
-SolverDynReduced('')
+from zmp_estimator import ZmpEstimator
+from robot_dyn_simu import RobotDynSimu
+from task_dyn_joint_limits import TaskDynJointLimits
+from task_dyn_limits import TaskDynLimits
+from task_joint_limits import TaskJointLimits
+from task_inequality import TaskInequality
+from feature_projected_line import FeatureProjectedLine
+from contact_selecter import ContactSelecter
+
+from task_weight import TaskWeight
+
 
 from solver_kine import SolverKine
-SolverKine('')
-
-from zmp_estimator import ZmpEstimator
-ZmpEstimator('')
-
-from robot_dyn_simu import RobotDynSimu
-RobotDynSimu('')
-
-from task_dyn_joint_limits import TaskDynJointLimits
-TaskDynJointLimits('')
-
-from task_dyn_limits import TaskDynLimits
-TaskDynLimits('')
-
-from task_joint_limits import TaskJointLimits
-TaskJointLimits('')
-
-from task_inequality import TaskInequality
-TaskInequality('')
-
-from feature_projected_line import FeatureProjectedLine
-FeatureProjectedLine('')
-
-from contact_selecter import ContactSelecter
-ContactSelecter('')
-
+def sot_addContact( sot,metaTask ):
+    metaTask.keep()
+    sot.push(metaTask.task.name)
+setattr(SolverKine,'addContact',sot_addContact)
